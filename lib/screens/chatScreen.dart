df2c01d1fe79891105f179d35c6e7dd66ae56ec9
HyperactiveX <s.chalanthorn@gmail.com>
2021-05-29 17:15:48 +0700

    [UPDATE] : Add listView to chatScreen

--- lib/screens/chatScreen.dart
+++ lib/screens/chatScreen.dart
@@ -1,3 +1,4 @@
+import 'package:dekcare_frontend/Screens/landingScreen.dart';
 import 'package:dekcare_frontend/components/chat/chatPreviewCard.dart';
 import 'package:dekcare_frontend/components/navBar/nav.dart';
 import 'package:dekcare_frontend/components/constants.dart';
@@ -11,6 +12,15 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatState extends State<ChatScreen> {
+  var refreshKey = GlobalKey<RefreshIndicatorState>();
+
+  Future<Null> refreshList() async {
+    refreshKey.currentState?.show(atTop: false);
+    await Future.delayed(Duration(seconds: 1));
+    setState(() {});
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
@@ -41,10 +51,40 @@ class _ChatState extends State<ChatScreen> {
                           child: searchBar(title: 'ค้นหาชื่อเพื่อน'),
                         ),
                       ),
-                      chatPreviewCard(
-                          name: "	นพ. วิรวรรธ ใจอารีย์",
-                          profile: "/assets/images/doctorIcon.png",
-                          message: "สวัสดีครับคนไข้")
+                      Container(
+                        height: height * 0.7,
+                        child: RefreshIndicator(
+                          key: refreshKey,
+                          onRefresh: refreshList,
+                          child: SingleChildScrollView(
+                            physics: ScrollPhysics(),
+                            child: Column(children: [
+                              ListView.builder(
+                                physics: NeverScrollableScrollPhysics(),
+                                shrinkWrap: true,
+                                itemCount: 100,
+                                itemBuilder: (context, index) =>
+                                    chatPreviewCard(
+                                  name: "	นพ. วิรวรรธ ใจอารีย์",
+                                  profile: "/assets/images/doctorIcon.png",
+                                  message: "สวัสดีครับคนไข้",
+                                  press: () {
+                                    print('navigate');
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) {
+                                          return LandingScreen();
+                                        },
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
+                            ]),
+                          ),
+                        ),
+                      ),
                     ],
                   ),
                 ),
