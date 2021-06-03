cbbb97d21af0263145979370a85319996da18f4c
Wirawat Jaiarree <katak6565@gmail.com>
2021-06-03 20:17:40 +0700

    [UPDATE] setting Screen

--- lib/screens/settingScreen.dart
+++ lib/screens/settingScreen.dart
@@ -1,6 +1,8 @@
 import 'package:dekcare_frontend/components/button.dart';
+import 'package:dekcare_frontend/components/buttonSetting.dart';
 import 'package:dekcare_frontend/components/constants.dart';
-import 'package:dekcare_frontend/components/navBar/nav.dart';
+import 'package:dekcare_frontend/components/text.dart';
+import 'package:dekcare_frontend/components/textBoxSetting.dart';
 import 'package:dekcare_frontend/provider/authenticateProvider.dart';
 import 'package:dekcare_frontend/screens/loginScreen.dart';
 
@@ -34,35 +36,83 @@ class _SettingState extends State<SettingScreen> {
     @override
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-            title: Text(
-              'setting',
-              style: TextStyle(fontSize: 23),
-            ),
-            centerTitle: true,
-            backgroundColor: yellowPrimary,
-            shape: RoundedRectangleBorder(
-                borderRadius:
-                    BorderRadius.vertical(bottom: Radius.circular(30)))),
-        body: Container(
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(
+            'setting',
+            style: TextStyle(fontSize: 23),
+          ),
+          centerTitle: true,
+          backgroundColor: yellowPrimary,
+          shape: RoundedRectangleBorder(
+              borderRadius:
+                  BorderRadius.vertical(bottom: Radius.circular(30)))),
+      body: SafeArea(
+        child: Container(
+          color: greySecondary,
           width: width,
-          child: Column(
-            children: [
-              SizedBox(
-                height: 30,
-              ),
-              Container(
-                child: Button(
-                  onPress: () {
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Stack(
+                      children: [
+                        Positioned(
+                            child: Container(
+                                width: width * 0.2,
+                                height: height * 0.2,
+                                child: CircleAvatar(
+                                  backgroundImage: NetworkImage(
+                                      'https://scontent.fkdt2-1.fna.fbcdn.net/v/t1.6435-9/84643335_3603241603051806_8217618763466407936_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=RmEYSc-U9uUAX-BMXey&_nc_ht=scontent.fkdt2-1.fna&oh=d752ff94b05a97243b0443f45fdb908a&oe=60DDA4AE'),
+                                ))),
+                        Positioned(
+                            top: 80,
+                            left: 50,
+                            child: Container(
+                                width: width * 0.07,
+                                height: height * 0.07,
+                                child: CircleAvatar(
+                                    backgroundColor: yellowPrimary,
+                                    child: Icon(
+                                      Icons.add,
+                                      color: whitePrimary,
+                                    ))))
+                      ],
+                    ),
+                  ),
+                ),
+                TextKT(text: 'บัญชี'),
+                TextBoxSetting(
+                  title: 'บัญชี',
+                  info: 'Wirawat09@gmail.com',
+                ),
+                TextBoxSetting(
+                  title: 'ชื่อ',
+                  info: 'Wirawat Jaiarree',
+                ),
+                TextKT(text: 'อื่นๆ'),
+                ButtonSetting(
+                  icon: Icons.lock,
+                  text: 'เปลี่ยนรหัสผ่าน',
+                ),
+                ButtonSetting(
+                  icon: Icons.headset_mic,
+                  text: 'ติดต่อเจ้าหน้าที่',
+                ),
+                ButtonSetting(
+                  icon: Icons.exit_to_app,
+                  text: 'ออกจากระบบ',
+                  onPressed: () {
                     logout();
                   },
-                  title: 'Logout',
-                  color: yellowPrimary,
                 ),
-              )
-            ],
+                SizedBox(
+                  height: 20,
+                ),
+              ],
+            ),
           ),
         ),
       ),
