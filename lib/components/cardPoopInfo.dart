1956e3b61380aa63168214c10c45f22dfaa1a29b
boyplus <ict224bj@gmail.com>
2021-06-04 19:17:30 +0700

    update: poop info csreen

--- lib/components/cardPoopInfo.dart
+++ lib/components/cardPoopInfo.dart
@@ -17,7 +17,36 @@ class _CardPoopInfoState extends State<CardPoopInfo> {
 
   Widget buildInfo(BuildContext context) {
     if (_isExpanded) {
-      return Container();
+      return Container(
+        // color: Colors.red,
+        padding: const EdgeInsets.only(left: 25),
+        child: Column(
+          children: [
+            ...widget.data.poops
+                .map(
+                  (e) => Row(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.only(right: 10, top: 0),
+                        width: 30,
+                        child: CircleAvatar(
+                          backgroundColor: HexColor(e.code),
+                        ),
+                      ),
+                      Text(
+                        e.text,
+                        style: TextStyle(
+                            fontFamily: 'supermarket',
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500),
+                      )
+                    ],
+                  ),
+                )
+                .toList()
+          ],
+        ),
+      );
     }
     return Container();
   }
@@ -26,115 +55,147 @@ class _CardPoopInfoState extends State<CardPoopInfo> {
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
-    return Container(
-      width: width * 0.9,
-      margin: EdgeInsets.only(top: 20),
-      child: DecoratedBox(
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(23),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey,
-                spreadRadius: 1,
-                blurRadius: 5,
-                offset: Offset(0, 5),
-              ),
-            ]),
-        child: Column(
-          children: [
-            SizedBox(height: height * 0.03),
-            Container(
-              padding: EdgeInsets.only(left: 20),
-              width: width,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    widget.data.date,
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(right: 10),
-                    child: Row(
-                      children: [
-                        ...widget.data.poops
-                            .map(
-                              (e) => Container(
-                                padding: const EdgeInsets.only(right: 10),
-                                width: 30,
-                                child: CircleAvatar(
-                                  backgroundColor: HexColor(e.code),
-                                ),
-                              ),
-                            )
-                            .toList(),
-                      ],
+    return InkWell(
+      onTap: () => setState(() => _isExpanded = !_isExpanded),
+      child: Container(
+        width: width * 0.9,
+        margin: EdgeInsets.only(top: 20),
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(23),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  spreadRadius: 1,
+                  blurRadius: 5,
+                  offset: Offset(0, 5),
+                ),
+              ]),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: height * 0.03),
+              Container(
+                padding: EdgeInsets.only(left: 20),
+                width: width,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      widget.data.date,
+                      style: TextStyle(fontSize: 20),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            buildInfo(context),
-            // Row(
-            //   children: [
-            // Container(
-            //   padding: const EdgeInsets.only(left: 20, top: 20, right: 20),
-            //   width: 100,
-            //   child: const CircleAvatar(
-            //     backgroundColor: yellowPrimary,
-            //   ),
-            // ),
-            //     Container(
-            //       padding: const EdgeInsets.only(top: 20, right: 20),
-            //       child: Text(
-            //         'เหลว+เหลือง',
-            //         style: const TextStyle(fontSize: 18),
-            //       ),
-            //     ),
-            //   ],
-            // ),
-            // Row(
-            //   children: [
-            //     Container(
-            //       padding: const EdgeInsets.only(left: 20, top: 20, right: 20),
-            //       width: 100,
-            //       child: const CircleAvatar(
-            //         backgroundColor: yellowPrimary,
-            //       ),
-            //     ),
-            //     Container(
-            //       padding: EdgeInsets.only(top: 20, right: 20),
-            //       child: Text(
-            //         'เหลว+เหลือง',
-            //         style: TextStyle(fontSize: 18),
-            //       ),
-            //     ),
-            //   ],
-            // ),
-            Container(
-              padding: const EdgeInsets.only(left: 25, top: 15, bottom: 15),
-              width: width,
-              child: widget.data.poops.length == 0
-                  ? Text(
-                      'ไม่มีข้อมูล',
-                      style: TextStyle(
-                          fontFamily: 'supermarket',
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500),
-                    )
-                  : Text(
-                      widget.data.result,
-                      style: TextStyle(
-                        fontFamily: 'supermarket',
-                        fontSize: 20,
-                        color: HexColor(widget.data.resultCode),
-                        fontWeight: FontWeight.w500,
+                    Container(
+                      margin: EdgeInsets.only(right: 10),
+                      child: Row(
+                        children: [
+                          ...widget.data.poops
+                              .map(
+                                (e) => Container(
+                                  padding: const EdgeInsets.only(right: 10),
+                                  width: 30,
+                                  child: CircleAvatar(
+                                    backgroundColor: HexColor(e.code),
+                                  ),
+                                ),
+                              )
+                              .toList(),
+                        ],
                       ),
                     ),
-            )
-          ],
+                  ],
+                ),
+              ),
+              buildInfo(context),
+              // Row(
+              //   children: [
+              // Container(
+              //   padding: const EdgeInsets.only(left: 20, top: 20, right: 20),
+              //   width: 100,
+              //   child: const CircleAvatar(
+              //     backgroundColor: yellowPrimary,
+              //   ),
+              // ),
+              //     Container(
+              //       padding: const EdgeInsets.only(top: 20, right: 20),
+              //       child: Text(
+              //         'เหลว+เหลือง',
+              //         style: const TextStyle(fontSize: 18),
+              //       ),
+              //     ),
+              //   ],
+              // ),
+              // Row(
+              //   children: [
+              //     Container(
+              //       padding: const EdgeInsets.only(left: 20, top: 20, right: 20),
+              //       width: 100,
+              //       child: const CircleAvatar(
+              //         backgroundColor: yellowPrimary,
+              //       ),
+              //     ),
+              //     Container(
+              //       padding: EdgeInsets.only(top: 20, right: 20),
+              //       child: Text(
+              //         'เหลว+เหลือง',
+              //         style: TextStyle(fontSize: 18),
+              //       ),
+              //     ),
+              //   ],
+              // ),
+              Container(
+                padding: const EdgeInsets.only(left: 25, top: 5, bottom: 15),
+                width: width,
+                child: widget.data.poops.length == 0
+                    ? Text(
+                        'ไม่มีข้อมูล',
+                        style: TextStyle(
+                            fontFamily: 'supermarket',
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500),
+                      )
+                    : Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10),
+                            child: Text(
+                              widget.data.result,
+                              style: TextStyle(
+                                fontFamily: 'supermarket',
+                                fontSize: 20,
+                                color: HexColor(widget.data.resultCode),
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                          _isExpanded
+                              ? IconButton(
+                                  padding: EdgeInsets.only(top: 0, right: 20),
+                                  constraints: BoxConstraints(),
+                                  onPressed: () {},
+                                  icon: Icon(
+                                    Icons.arrow_drop_up,
+                                    size: 40,
+                                  ),
+                                )
+                              : IconButton(
+                                  padding: EdgeInsets.only(top: 0, right: 20),
+                                  constraints: BoxConstraints(),
+                                  onPressed: () {},
+                                  icon: Icon(
+                                    Icons.arrow_drop_down,
+                                    size: 40,
+                                  ),
+                                ),
+                        ],
+                      ),
+              )
+            ],
+          ),
         ),
       ),
     );
