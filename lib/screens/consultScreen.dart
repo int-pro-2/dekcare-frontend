66f1f9775db57179907b351966fb4f40fa0fb657
HyperactiveX <s.chalanthorn@gmail.com>
2021-06-05 15:27:11 +0700

    [UPDATE] : Update UI in chat & consult screens

--- lib/screens/consultScreen.dart
+++ lib/screens/consultScreen.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:dekcare_frontend/screens/individualChatScreen.dart';
 import 'package:dekcare_frontend/components/Toggle.dart';
 import 'package:dekcare_frontend/components/constants.dart';
@@ -24,6 +22,9 @@ class _ConsultState extends State<ConsultScreen> {
   Future<Null> refreshList() async {
     refreshKey.currentState?.show(atTop: false);
     await Future.delayed(Duration(seconds: 1));
+    setState(() {
+      isLoading = true;
+    });
     ListDoctor(widget.fav);
     return null;
   }
@@ -67,7 +68,7 @@ class _ConsultState extends State<ConsultScreen> {
           appBar: PreferredSize(
             child: TopBar(
               titleText: 'ปรึกษาลูกน้อยกับหมอ',
-              enableBackButton: true,
+              enableBackButton: false,
               contextFromPage: context,
             ),
             preferredSize: Size.fromHeight((height * 0.075)),
@@ -86,13 +87,13 @@ class _ConsultState extends State<ConsultScreen> {
                     current: widget.fav,
                     onChange: changeFav,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 10.0),
-                    child: Container(
-                      width: width * 0.92,
-                      child: searchBar(title: 'ค้นหาชื่อคุณหมอที่นี่'),
-                    ),
-                  ),
+                  // Padding(
+                  //   padding: const EdgeInsets.only(bottom: 10.0),
+                  //   child: Container(
+                  //     width: width * 0.92,
+                  //     child: searchBar(title: 'ค้นหาชื่อคุณหมอที่นี่'),
+                  //   ),
+                  // ),
                   Expanded(
                     child: RefreshIndicator(
                       key: refreshKey,
